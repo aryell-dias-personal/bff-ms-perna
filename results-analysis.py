4c4fb73cd93162b4bac29c601d6d42c613ed4230
Aldo Monteiro <aldo.monteiro@pitang.com>
2020-08-28 18:38:55 -0300

    Mapa com rotas e curva de convergênca

--- results-analysis.py
+++ results-analysis.py
@@ -6,7 +6,7 @@ import plotly.express as px
 
 from src.services.ant_colony import AntSystem
 from src.services.genetic_algorithm import GeneticAlgorithm
-from src.helpers.constants import MATRIX_FIELDS, DB_COLLECTIONS
+from src.helpers.constants import MATRIX_FIELDS, DB_COLLECTIONS, ENCODED_NAMES
 from src.helpers.transform import binaryTroughtMatrix
 import base64
 import json
@@ -17,12 +17,14 @@ def getArgs():
         event = json.load(f)
         return json.loads(base64.b64decode(event['data']).decode('utf-8'))
 
+bestFitness = []
 def geneticFitnessCallback(data):
-    global progressBar
+    global progressBar, bestFitness
     iteration = data.get('iteration')
     numIter = data.get('numIter')
     fitnessValues = data.get('fitnessValues')
     progressBar.progress((iteration+1)/numIter)
+    bestFitness.append(max(fitnessValues))
 
 
 def getRoutes(geneticSystemArgs):
@@ -33,125 +35,142 @@ def getRoutes(geneticSystemArgs):
     result = geneticSystem.decodeChromosome(geneticSystem.population[0])
     return result
 
-geneticSystemArgs = getArgs()
-geneticSystemArgs
+def decodePlace(place):
+    strPlace = place.split(ENCODED_NAMES.SEPARETOR)[0]
+    return [float(coord) for coord in strPlace.split(',')][::-1]
 
-# st.write(
-#     px.line(
-#         [1,2,3]
-#     )
-# )
+def parsePoint(point):
+    return { **point, 'local': [float(coord) for coord in point['local'].split(',')][::-1] }
+
+def parseRoute(route):
+    return [parsePoint(point) for point in route]
+
+geneticSystemArgs = getArgs()
+# geneticSystemArgs
 
 progressBar = st.progress(0)
 routes = getRoutes(geneticSystemArgs)
-routes
+# routes
 
-data = pd.DataFrame({
-    'awesome cities' : ['Chicago', 'Minneapolis', 'Louisville', 'Topeka'],
-    'lat' : [41.868171, 44.979840,  38.257972, 39.030575],
-    'lon' : [-87.667458, -93.272474, -85.765187,  -95.702548]
-})
-
-def buildLine(row) :
-    return [
-        {
-            # inbound: 72633,
-            # outbound: 74735,
-            'from': {
-            'name': '19th St. Oakland (19TH)',
-            'coordinates': [-122.269029, 37.80787]
-            },
-            'to': {
-            'name': '12th St. Oakland City Center (12TH)',
-            'coordinates': [-122.271604, 37.803664]
-            }
-        }
-    ]
-
-path = []
-for i, (_, current) in enumerate(data.iterrows()):
-    nextRow = data.iloc[i+1] if i < len(data) - 1 else data.iloc[0]
-    path.append(
-        {
-            # inbound: 72633,
-            # outbound: 74735,
-            'from': {
-                # 'name': '19th St. Oakland (19TH)',
-                'coordinates': [current.lat, current.lon]
-            },
-            'to': {
-                # 'name': '12th St. Oakland City Center (12TH)',
-                'coordinates': [nextRow.lat, nextRow.lon]
-            }
-        }
-    )
-
-# Adding code so we can have map default to the center of the data
-midpoint = (np.average(data['lat']), np.average(data['lon']))
-
-st.deck_gl_chart(
-    viewport={
-        'latitude': midpoint[0],
-        'longitude':  midpoint[1],
-        'zoom': 4
-    },
-    layers=[
-        {
-            'type': 'ScatterplotLayer',
-            'data': data,
-            'radiusScale': 250,
-            'radiusMinPixels': 5,
-            'getFillColor': [248, 24, 148],
-        },
-        {
-            'type': 'LineLayer',
-            'data': path,
-            'getWidth': 1,
-            'getSourcePosition': 'from:{coordinates}',
-            'getTargetPosition': 'to:{coordinates}',
-        },
-        
-    ]
+st.write(
+    px.line(bestFitness)
 )
 
+# allPoints = pd.DataFrame({
+#     'awesome cities' : ['Chicago', 'Minneapolis', 'Louisville', 'Topeka'],
+#     'lat' : [41.868171, 44.979840,  38.257972, 39.030575],
+#     'lon' : [-87.667458, -93.272474, -85.765187,  -95.702548]
+# })
+ORIGINS_COLOR = [24, 248, 148]
+DESTINATIONS_COLOR = [248, 24, 50]
+origins = []
+destinations = []
+for askedPoint in geneticSystemArgs['matrix']['askedPoints']:
+    originPoint = decodePlace(askedPoint['origin'])
+    destinyPoint = decodePlace(askedPoint['destiny'])
+    origins.append(dict(
+        lat=originPoint[0],
+        lon=originPoint[1],
+        color=ORIGINS_COLOR
+    ))
+    destinations.append(dict(
+        lat=destinyPoint[0],
+        lon=destinyPoint[1],
+        color=DESTINATIONS_COLOR
+    ))
 
+origins = pd.DataFrame(origins)
+destinations = pd.DataFrame(destinations)
+allPoints = pd.concat([origins, destinations], axis=0, ignore_index=True)
+allPoints
 agg_route_paths = [
     {
-        "path": [
-            [
-                2.0297719,
-                41.3911309
-            ],
-            [
-                -0.4111182,
-                39.528239299999996
-            ],
-            [
-                -0.4111182,
-                39.528239299999996
-            ],
-        ]
+        'path': [point['local'] for point in parseRoute(data['route'])]
     }
-]
+for data in routes ]
+
+# Adding code so we can have map default to the center of the data
+midpoint = (allPoints.lat.mean(), allPoints.lon.mean())
+midpoint
+
+# st.deck_gl_chart(
+#     viewport={
+#         'latitude': midpoint[0],
+#         'longitude':  midpoint[1],
+#         'zoom': 4
+#     },
+#     layers=[
+#         {
+#             'type': 'ScatterplotLayer',
+#             'data': data,
+#             'radiusScale': 250,
+#             'radiusMinPixels': 5,
+#             'getFillColor': [248, 24, 148],
+#         },
+#         {
+#             'type': 'LineLayer',
+#             'data': path,
+#             'getWidth': 1,
+#             'getSourcePosition': 'from:{coordinates}',
+#             'getTargetPosition': 'to:{coordinates}',
+#         },
+        
+#     ]
+# )
+
+# agg_route_paths = [
+#     {
+#         "path": [
+#             [
+#                 2.0297719,
+#                 41.3911309
+#             ],
+#             [
+#                 -0.4111182,
+#                 50
+#             ],
+#             [
+#                 -0.4111182,
+#                 39.528239299999996
+#             ],
+#         ]
+#     }
+# ]
 
 view_state = pdk.ViewState(
-    latitude=midpoint[0],
-    longitude=midpoint[1],
-    zoom=4
+    latitude=midpoint[1],
+    longitude=midpoint[0],
+    zoom=7
 )
 
-layer = pdk.Layer(
+print(agg_route_paths)
+pointsLayer = pdk.Layer(
+    type = 'ScatterplotLayer',
+    data = allPoints,
+    radiusScale = 250,
+    radiusMinPixels = 5,
+    get_position=['lat', 'lon'],
+    getFillColor = 'color',
+)
+# destinationsLayer = pdk.Layer(
+#     type = 'ScatterplotLayer',
+#     data = destinations,
+#     radiusScale = 250,
+#     radiusMinPixels = 5,
+#     get_position=['lat', 'lon'],
+#     getFillColor = [248, 24, 50],
+# )
+pathLayer = pdk.Layer(
     type='PathLayer',
     data=agg_route_paths,
     pickable=True,
-    get_color='color',
+    get_color=[248, 24, 148],
     width_scale=20,
     width_min_pixels=2,
     get_path='path',
     get_width=5
 )
-
-r = pdk.Deck(layers=[layer], initial_view_state=view_state)
+layers = [pathLayer, pointsLayer]
+r = pdk.Deck(layers=layers, initial_view_state=view_state)
 
 st.pydeck_chart(r)
-
