e293f01b008e71263d66bf99000cd34e13652644
aryelld <aryell@gmail.com>
2021-02-08 22:41:23 -0300

    ajustes nos console.log

--- src/helpers/error-handler.js
+++ src/helpers/error-handler.js
@@ -2,13 +2,14 @@ const { RETURN_MESSAGES } = require('../helpers/constants');
 
 module.exports.handler = async (req, res, func) => {
     try {
-        console.log("BODY: \n" + req.body);
         const body = JSON.parse(req.body);
+        console.log("BODY: \n", body);
         const data = await func(body);
         const response = { message: RETURN_MESSAGES.SUCCESS, ...data };
+        console.log("FINAL RESULT \n", { response });
         res.status(200).send(response);
     } catch (error) {
-        console.log(`ERROR: \n ${error}`);
+        console.log("ERROR: \n", error);
         res.status(500).send({
             message: RETURN_MESSAGES.ERROR,
             error: error.message
@@ -16,17 +17,31 @@ module.exports.handler = async (req, res, func) => {
     }
 }
 
+module.exports.eventHandler =  async (event, context) => {
+    try {
+        console.log("EVENT: \n", { event, context });
+        const body = getInfoFromPubSubEvent(event);
+        console.log("BODY: \n", body);
+        const data = await func(body);
+        const response = { message: RETURN_MESSAGES.SUCCESS, ...data };
+        console.log("FINAL RESULT \n", { response });
+    } catch (error) {
+        console.log("ERROR: \n", error);
+    }
+};  
+
 module.exports.authHandler = async (req, res, func) => {
     try {
-        console.log("BODY: \n" + req.body);
         const body = JSON.parse(req.body);
+        console.log("BODY: \n", body);
         const headers = req.headers;
-        console.log("HEADERS: \n" + JSON.stringify(req.headers));
+        console.log("HEADERS: \n", req.headers);
         const data = await func(body, headers.authorization);
         const response = { message: RETURN_MESSAGES.SUCCESS, ...data };
+        console.log("FINAL RESULT \n", { response });
         res.status(200).send(response);
     } catch (error) {
-        console.log(`ERROR: \n ${error}`);
+        console.log("ERROR: \n", error);
         res.status(500).send({
             message: RETURN_MESSAGES.ERROR,
             error: error.message
